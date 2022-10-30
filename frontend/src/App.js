d15659a0cad39a6b51d218d7e4b6164197a842cf
VAMSHI NENAVATH <nenavath_b190217cs@nitc.ac.in>
2022-10-30 22:09:10 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">nitc ecommerce</a>
+        <main>list products</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
